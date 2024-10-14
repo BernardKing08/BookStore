d6f277408f57ca2806e06f861bf02df73d6e9ef9
BernardKing08 <bernardkingsley08@gmail.com>
2024-10-14 16:35:36 +0100

    Password hashing using BCryptPasswordEncoder

--- OnlineBookStore2/src/main/java/com/OnlineBookStore/Service/PersonService.java
+++ OnlineBookStore2/src/main/java/com/OnlineBookStore/Service/PersonService.java
@@ -1,6 +1,7 @@
 package com.OnlineBookStore.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.OnlineBookStore.Model.Roles;
 import com.OnlineBookStore.Model.Person;
@@ -19,11 +20,19 @@ public class PersonService {
 	@Autowired
 	private RolesRepository rolesRepository; 
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	
+	
 	public boolean createNewPerson(Person person) {
 		boolean isSaved = false;
 		
 		Roles role = rolesRepository.getByRoleName(OnlineBookStoreConstants.STUDENT_ROLE);
 		person.setRoles(role);
+		
+		person.setPwd(passwordEncoder.encode(person.getPwd()));
+		
 		person = personRepository.save(person);
 		
         if (null != person && person.getPersonId() > 0)
